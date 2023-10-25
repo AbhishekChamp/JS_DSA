260b8a328e847210538f8e78d640e6bac740cd8c
AbhishekChamp <abhishek96.r@gmail.com>
2023-10-25 06:48:13 +0530

    Day - 02

--- 01_Frequency_Counters/01_same_solution.js
+++ 01_Frequency_Counters/01_same_solution.js
@@ -1,7 +1,9 @@
 // Question
 
 /*
-Write a function called same, which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be the same.
+Write a function called same, which accepts two arrays. 
+The function should return true if every value in the array has it's corresponding value squared in the second array. 
+The frequency of values must be the same.
 */
 
 function same(arr1, arr2) {
