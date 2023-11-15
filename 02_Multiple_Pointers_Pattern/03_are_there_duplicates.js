1c48172d46c368a33768c25ccf02b6c62dcfa6f6
AbhishekChamp <abhishek96.r@gmail.com>
2023-11-15 21:26:16 +0530

    Day - 05

---
+++ 02_Multiple_Pointers_Pattern/03_are_there_duplicates.js
@@ -0,0 +1,24 @@
+// Question
+
+/*
+Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+*/
+
+// Example
+
+/*
+areThereDuplicates(1, 2, 3) // false
+areThereDuplicates(1, 2, 2) // true 
+areThereDuplicates('a', 'b', 'c', 'a') // true 
+*/
+
+function areThereDuplicates(...args) {
+    return new Set(args).size !== args.length;
+}
+
+console.log(areThereDuplicates(1, 2, 3));
+console.log(areThereDuplicates(1, 2, 2));
+console.log(areThereDuplicates("a", "b", "c", "a"));
+
+// Time Complexity - O(N log N)
+// Space Complexity - O(1)
