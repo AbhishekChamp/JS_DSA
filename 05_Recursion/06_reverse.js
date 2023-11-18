2c5a8feff8188eee9b2fda492b4b1b3091d6b3e5
AbhishekChamp <abhishek96.r@gmail.com>
2023-11-19 02:10:21 +0530

    Day - 09

---
+++ 05_Recursion/06_reverse.js
@@ -0,0 +1,18 @@
+/*
+Write a recursive function called reverse which accepts a string and returns a new string in reverse.
+*/
+
+// Example
+
+/*
+reverse('awesome') // 'emosewa'
+reverse('rithmschool') // 'loohcsmhtir'
+*/
+
+function reverse(str) {
+    if (str.length <= 1) return str;
+    return reverse(str.slice(1)) + str[0];
+}
+
+console.log(reverse("awesome"));
+console.log(reverse("rithmschool"));
