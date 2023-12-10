52aee9bb69502f0203023ca40d000d083cee04e7
AbhishekChamp <abhishek96.r@gmail.com>
2023-12-10 05:59:45 +0530

    Day - 21

---
+++ 09_Singly_Linked_List/01_singly_linked_list.js
@@ -0,0 +1,51 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val) {
+        const newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    pop() {
+        if (!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return this;
+    }
+}
+
+const list = new SinglyLinkedList();
+console.log(list);
+console.log(list.pop());
+console.log(list.push("Hi"));
+console.log(list.push("Hello"));
+console.log(list.pop());
+console.log(list.pop());
