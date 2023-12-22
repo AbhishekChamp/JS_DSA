0ad3aabf12a4bcbed69288ee2c0699b2f022e20e
AbhishekChamp <abhishek96.r@gmail.com>
2023-12-23 03:50:51 +0530

    Day - 27

--- 10_Doubly_Linked_List/01_doubly_linked_list.js
+++ 10_Doubly_Linked_List/01_doubly_linked_list.js
@@ -71,6 +71,70 @@ class DoublyLinkedList {
         return this;
     }
 
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+        let count, current;
+        if (index <= this.length / 2) {
+            count = 0;
+            current = this.head;
+            while (count !== index) {
+                current = current.next;
+                count++;
+            }
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while (count !== index) {
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+    }
+
+    set(index, val) {
+        const currentNode = this.get(index);
+        if (!currentNode) return false;
+        currentNode.val = val;
+        return true;
+    }
+
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false;
+        if (index === 0) return !!this.unshift(val);
+        if (index === this.length) return !!this.push(val);
+
+        const prevNode = this.get(index - 1);
+        const nextNode = prevNode.next;
+        const newNode = new Node(val);
+
+        prevNode.next = newNode;
+        newNode.prev = prevNode;
+
+        newNode.next = nextNode;
+        nextNode.prev = newNode;
+        this.length++;
+        return true;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+
+        const removeNode = this.get(index);
+        const prevNode = removeNode.prev;
+        const nextNode = removeNode.next;
+
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+        removeNode.next = null;
+        removeNode.prev = null;
+
+        this.length--;
+        return removeNode;
+    }
+
     print() {
         const arr = [];
         let current = this.head;
@@ -97,4 +161,26 @@ console.log(DDL.print());
 DDL.shift();
 console.log(DDL.print());
 DDL.unshift(1);
+console.log(DDL.print());
+DDL.push(30);
+DDL.push(40);
+console.log(DDL.print());
+console.log(DDL.get(-1));
+console.log(DDL.get(10));
+console.log(DDL.get(2));
+console.log(DDL.get(4));
+DDL.set(1, 10);
+DDL.set(-1, 100);
+console.log(DDL.print());
+DDL.insert(0, 0);
+console.log(DDL.print());
+DDL.insert(6, 50);
+console.log(DDL.print());
+DDL.insert(2, 5);
+console.log(DDL.print());
+DDL.remove(0);
+console.log(DDL.print());
+DDL.remove(6);
+console.log(DDL.print());
+DDL.remove(1);
 console.log(DDL.print());
--- 10_Doubly_Linked_List/README.md
+++ 10_Doubly_Linked_List/README.md
@@ -59,3 +59,64 @@ Adding a node to the beginning of the Doubly Linked List
     -   Update the head to be the new node
 -   Increment the length
 -   Return the list
+
+### Get
+
+Accessing a node in a Doubly Linked List by its position
+
+### Get Pseudocode
+
+-   If the index is less than 0 or greater or equal to the length, return null
+-   If the index is less than or equal to half the length of the list
+    -   Loop through the list starting from the head and loop towards the middle
+    -   Return the node once it is found
+-   If the index is greater than half the length of the list
+    -   Loop through the list starting from the tail and loop towards the middle
+    -   Return the node once it is found
+
+### Set
+
+Replacing the value of a node to the index of it in a Doubly Linked List
+
+### Set Pseudocode
+
+-   Create a variable which is the result of the get method at the index passed to the function
+    -   If the get method returns a valid node, set the value of that node to be the value passed to the function
+    -   Return true
+-   Otherwise, return false
+
+### Insert
+
+Adding a node in a Doubly Linked List by a certain position
+
+### Insert Pseudocode
+
+-   If the index is less than zero or greater than or equal to the length return false
+-   If the index is 0, unshift
+-   If the index is the same as the length, push
+-   Use the get method to access the index-1
+-   Set the next and prev properties on the correct nodes to link everything together
+-   Increment the length
+-   Return true
+
+### Remove
+
+Removing a node in a Doubly Linked List by a certain position
+
+### Remove Pseudocode
+
+-   If the index is less than zero or greater then or equal to the length return undefined
+-   If the index is 0, shift
+-   If the index is the same as the length-1, pop
+-   Use the get method to retrieve the item to be removed
+-   Update the next and prev properties to remove the found node from the list
+-   Set next and prev to null on the found node
+-   Decrement the length
+-   Return the removed node
+
+### Big O
+
+-   Insertion - O(1)
+-   Removal - O(1)
+-   Searching - O(N)
+-   Access - O(N)
