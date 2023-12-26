3cc60fa50a7b729c0558ffb5fe0b80e7c82284ef
AbhishekChamp <abhishek96.r@gmail.com>
2023-12-27 05:12:33 +0530

    Day - 31

---
+++ 14_Tree_Traversal/02_Depth_First_Search_PreOrder.js
@@ -0,0 +1,117 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+    insert(val) {
+        const newNode = new Node(val);
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+            while (true) {
+                if (val === current.val) return undefined;
+                if (val < current.val) {
+                    if (!current.left) {
+                        current.left = newNode;
+                        return this;
+                    }
+                    current = current.left;
+                } else if (val > current.val) {
+                    if (!current.right) {
+                        current.right = newNode;
+                        return this;
+                    }
+                    current = current.right;
+                }
+            }
+        }
+    }
+    find(val) {
+        if (!this.root) return undefined;
+        if (this.root.val === val) return this.root;
+        let temp = this.root;
+        while (true) {
+            if (temp.val === val) return temp;
+            if (temp.val < val) {
+                if (!temp.right) return undefined;
+                temp = temp.right;
+            } else {
+                if (!temp.left) return undefined;
+                temp = temp.left;
+            }
+        }
+    }
+    contains(val) {
+        if (!this.root) return false;
+        if (this.root.val === val) return true;
+        let temp = this.root;
+        while (true) {
+            if (temp.val === val) return true;
+            if (temp.val < val) {
+                if (!temp.right) return false;
+                temp = temp.right;
+            } else {
+                if (!temp.left) return false;
+                temp = temp.left;
+            }
+        }
+    }
+    BFS() {
+        let node = this.root;
+        const data = [];
+        const queue = [];
+        queue.push(node);
+
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
+    DFS() {
+        const data = [];
+        function traverse(node) {
+            data.push(node.val);
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+}
+
+//         10
+//      5       13
+//  2     7    11   16
+
+const BST = new BinarySearchTree();
+console.log(BST.find(5));
+BST.insert(10);
+console.log(BST.find(10));
+BST.insert(5);
+BST.insert(13);
+BST.insert(2);
+BST.insert(7);
+BST.insert(16);
+BST.insert(11);
+BST.insert(6);
+console.log(BST);
+console.log(BST.find(1));
+console.log(BST.find(6));
+console.log(BST.find(13));
+console.log(BST.contains(1));
+console.log(BST.contains(6));
+console.log(BST.contains(13));
+console.log(BST.BFS());
+console.log(BST.DFS());
---
+++ 14_Tree_Traversal/README.md
@@ -0,0 +1,33 @@
+### Traversing A Tree
+
+There are two ways:
+
+-   Breadth-First Search
+-   Depth-First Search
+    -   PreOrder
+    -   PostOrder
+    -   InOrder
+
+### Breadth-First Search
+
+##### Steps - Iteratively
+
+-   Create a queue (this can be an array) and a variable to store the values of nodes visited
+-   Place the root node in the queue
+-   Loop as long as there is anything in the queue
+    -   Dequeue a node from the queue and push the value of the node into the variable that stores the nodes
+    -   If there is a left property on the node dequeued - add it to the queue
+    -   If there is a right property on the node dequeued - add it to the queue
+
+### Depth-First Search [PreOrder]
+
+##### Steps - Iteratively
+
+-   Create a variable to store the values of nodes visited
+-   Store the root of the BST in a variable called current
+-   Write a helper function which accepts a node
+    -   Push the value of the node to the variable that stores the values
+    -   If the node has a left property, call the helper function with left property on the node
+    -   If the node has a right property, call the helper function with right property on the node
+-   Invoke the helper function with the current variable
+-   Return the array of values
